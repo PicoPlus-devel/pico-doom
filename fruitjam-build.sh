d0000f20336410e3b66cb3cb7fa739c965d9a48b
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2026-07-05 11:36:19 +0200

    remove bootloader build flag and update output address for picotool conversion

--- fruitjam-build.sh
+++ fruitjam-build.sh
@@ -10,7 +10,6 @@ cmake -S . -B $BUILD \
     -DPICOTOOL_FETCH_FROM_GIT_PATH="$(pwd)/picotool" \
     -DBOARD=adafruit_fruit_jam -DPICO_BOARD=adafruit_fruit_jam \
     -DUSE_HSTX=1 \
-    -DBUILD_FOR_BOOTLOADER=ON \
     ${CMAKE_ARGS} "$@"
 
 make -C $BUILD -j$(nproc)
@@ -26,4 +25,4 @@ fi
 # pico-bootLoader flash map: WHX lives at 0x10400000 (past the 3 MB slot
 # reserved for the doom_tiny image at 0x10100000). Must match TINY_WAD_ADDR
 # in fruitjam_cflags.h (the BUILD_FOR_BOOTLOADER branch).
-"$PICOTOOL" uf2 convert doom1.whx -t bin $BUILD/src/doom1-whx-for-fruitjam.uf2 -o 0x10400000 --family data
+"$PICOTOOL" uf2 convert doom1.whx -t bin $BUILD/src/doom1-whx-for-fruitjam.uf2 -o -o 0x10080000 --family data
